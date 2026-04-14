732fcd8397013f4ad56f226e249e8b6bc0e00ed2
u <123@example.com>
2026-04-14 17:33:54 +0800

    xiugai

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
